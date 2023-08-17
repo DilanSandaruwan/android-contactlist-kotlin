a9f9452ecf0d3e235ff8239a7e862e1009ad057d
DilanSandaruwan <mnd.sanda94@gmail.com>
2023-08-17 22:15:14 +0530

    create contacts listing screen

---
+++ app/src/main/java/com/dilan/example/android/ctcontactlistapplication/views/activities/ContactListActivity.kt
@@ -0,0 +1,23 @@
+package com.dilan.example.android.ctcontactlistapplication.views.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.dilan.example.android.ctcontactlistapplication.R
+import com.dilan.example.android.ctcontactlistapplication.databinding.ActivityContactListBinding
+
+class ContactListActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityContactListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_contact_list)
+
+        binding.btnArrowBack.setOnClickListener{
+            startActivity(Intent(this@ContactListActivity,MainActivity::class.java))
+            this.finish()
+        }
+    }
+}
--- app/src/main/java/com/dilan/example/android/ctcontactlistapplication/views/activities/MainActivity.kt
+++ app/src/main/java/com/dilan/example/android/ctcontactlistapplication/views/activities/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.dilan.example.android.ctcontactlistapplication.views.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.dilan.example.android.ctcontactlistapplication.R
+import com.dilan.example.android.ctcontactlistapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnContact.setOnClickListener {
+            val intent = Intent(this@MainActivity, ContactListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
