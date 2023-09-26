621b0bbe0fd67a7581864c8ddd961f055ced41a0
DilanSandaruwan <mnd.sanda94@gmail.com>
2023-09-26 23:28:24 +0530

    remove commented test cases for DetailsFragment

--- app/src/test/java/com/dilan/example/android/ctcontactlistapplication/views/fragments/DetailsFragmentTest.kt
+++ app/src/test/java/com/dilan/example/android/ctcontactlistapplication/views/fragments/DetailsFragmentTest.kt
@@ -4,33 +4,4 @@ internal class DetailsFragmentTest {
 
 //  TODO : with navigation and fragments
 
-//    @Mock
-//    private lateinit var view: TextInputEditText
-//
-//    lateinit var fragment: DetailsFragment
-//
-//    @Before
-//    fun setUp() {
-//        MockitoAnnotations.initMocks(this)
-//        fragment = DetailsFragment()
-//    }
-//
-//    @Test
-//    fun testEmailValidity() {
-//        // Define a valid email address
-//        val validEmail = "validemail@example.com"
-//
-//        // Mock the behavior of the TextInputEditText
-//        Mockito.`when`(view.text).thenReturn(Editable.Factory.getInstance().newEditable(validEmail))
-//
-//        // Call the validateEmail function
-//        val result = fragment.validateEmail(view)
-//
-//        // Verify that the function returns true for a valid email
-//        assertTrue(result)
-//
-//        // Verify that the TextInputEditText's error is cleared and it's disabled
-//        Mockito.verify(view).error = null
-//        Mockito.verify(view).isEnabled = false
-//    }
 }
